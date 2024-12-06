44ebdcac403b67d9ddb356e503456936966002a7
suurajroshan <roshasu@usi.ch>
2024-12-06 11:25:31 +0100

    log files

--- ddpm-torch/sbatch_train.sh
+++ ddpm-torch/sbatch_train.sh
@@ -8,13 +8,14 @@
 
 # conda activate sd
 
-python train.py \
+python3.9 train.py \
     --chkpt-dir="./chkpts/ddim" \
     --use-ddim \
     --eval \
     --exp-name="ddim" \
     --dataset="cifar10" \
     --batch-size=256 \
+<<<<<<< Updated upstream
     --num-samples=128 \
     --root="/home/roshasu/atml/datasets" \
     --eval-total-size=4096 \
@@ -22,3 +23,9 @@ python train.py \
     --image-intv=5 \
     --chkpt-intv=3900 \
     --immiscibility 
+=======
+    --num-samples=64 \
+    --chkpt-intv=120 \
+    --immiscibility \
+    --dry-run 
+>>>>>>> Stashed changes
